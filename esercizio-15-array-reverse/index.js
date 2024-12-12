c0a0df9d54317974d724dabc6b635e132abe9d98
marinademinescu <demi.marina@yahoo.it>
2024-12-12 14:59:42 +0100

    es-15-array-reverse

---
+++ esercizio-15-array-reverse/index.js
@@ -0,0 +1,8 @@
+let  numeri = [10, 20, 30, 40];
+let arrayReverse = []
+
+for (let i = numeri.length -1; i >= 0; i--) {
+    arrayReverse.push(numeri[i]);
+}
+
+console.log("Array invertito: ", arrayReverse);
