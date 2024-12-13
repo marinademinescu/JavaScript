ac5a1ef9f782fce25fe90ded93e1810c514dc270
marinademinescu <demi.marina@yahoo.it>
2024-12-13 21:15:58 +0100

    es-16-pop-array

---
+++ esercizio-16-popolamento-array/index.js
@@ -0,0 +1,12 @@
+let parole = [];
+
+for (let i = 0; i < 5; i++) {
+    let parola = prompt("Inserisci una parola:");
+    parole.push(parola);
+}
+
+for (let i = 0; i < parole.length; i++) {
+    if (parole[i].length % 2 !== 0) {
+        console.log(parole[i]);
+    }
+}
