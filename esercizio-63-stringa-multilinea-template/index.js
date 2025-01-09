619cace432926ba58f201dd0fb41de975e300af7
marinademinescu <demi.marina@yahoo.it>
2025-01-09 18:04:21 +0100

    es-63-multilinea-template

---
+++ esercizio-63-stringa-multilinea-template/index.js
@@ -0,0 +1,13 @@
+const nome = "Marina"
+const cognome = "Demi"
+const eta = 34
+const citta = "Venezia"
+
+const info =  `
+Nome: ${nome}
+Cognome: ${cognome}
+Eta: ${eta}
+Citta: ${citta}
+`;
+
+console.log(info)
