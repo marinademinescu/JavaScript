c63efa9f85f65f6c752204b12d3551401b23c4ca
marinademinescu <demi.marina@yahoo.it>
2025-01-09 22:50:40 +0100

    es-65-console-log

---
+++ esercizio-65-utilizzare-console-log/index.js
@@ -0,0 +1,13 @@
+const numero = 24
+const stringa = "stringa"
+const array = [1, 2, 3]
+const oggetto = {
+    nome: "Marina",
+    eta: 23,
+    citta: "Verona"
+}
+
+console.log(numero) 
+console.log(stringa)
+console.log(array)
+console.log(oggetto)
