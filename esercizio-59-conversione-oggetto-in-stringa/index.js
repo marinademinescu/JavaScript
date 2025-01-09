50f5faf890afd52b3b1c10e499f5af6191a3e8b2
marinademinescu <demi.marina@yahoo.it>
2025-01-09 16:37:24 +0100

    es-59-oggetto-in-stringa

---
+++ esercizio-59-conversione-oggetto-in-stringa/index.js
@@ -0,0 +1,11 @@
+const persona = {
+    nome: "Mario",
+    cognome: "Rossi",
+    eta: 30,
+    citta: "Milano",
+    mansione: "pizzaiolo"
+}
+
+const personaJSON = JSON.stringify(persona)
+
+console.log(personaJSON)
