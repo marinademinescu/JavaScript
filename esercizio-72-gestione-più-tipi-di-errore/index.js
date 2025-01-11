74302c69a18d2f5cd77d2ee20caa69ee6cf3feb7
marinademinescu <demi.marina@yahoo.it>
2025-01-11 12:57:50 +0100

    es-72-+-tipi-errore

---
+++ esercizio-72-gestione-più-tipi-di-errore/index.js
@@ -0,0 +1,26 @@
+function operazioniSuStringa (str) {
+    try{
+        if (typeof str !== 'string') {
+            throw new TypeError("La variabile non è una stringa ")
+        }
+
+        const maisc = str.toUpperCase()
+        console.log("Stringa in maiuscolo:", maisc)
+
+    
+    } catch (error){
+        if(error instanceof TypeError) {
+            console.error("Errore di tipo:", error.message)
+         } else if (error instanceof ReferenceError) { 
+            console.error("Errore di riferimento:", error.message)
+        
+        } else {
+            console.error("Errore sconosciuto:", error.message )
+        }
+    }
+   
+}
+
+operazioniSuStringa(123)
+
+operazioniSuStringa("stringa in maiuscolo!")
