14424b46d7ab5e35bd6f5a9c828b9c7b7cb2ffef
marinademinescu <demi.marina@yahoo.it>
2025-01-22 12:34:06 +0100

    es-100-session-storage

---
+++ esercizio-100-session-storage/index.js
@@ -0,0 +1,27 @@
+const key = "nome"
+const value = "Marina"
+
+const sessionStorageManager = {
+    save: (key, value) => {
+        sessionStorage.setItem(key, value)
+        console.log("Valore salvato", value)
+    },
+
+    retrieve: (key) => {
+        const retrieve = sessionStorage.getItem(key)
+        if(retrieve) {
+            console.log("Valore recuperato", retrieve )
+        } else {
+            console.log("Nessun valore trovato per la chiave", key)
+        }
+    },
+
+    remove: (key) => {
+        sessionStorage.removeItem(key)
+        console.log("Valore rimosso:", key)
+    }
+}
+
+sessionStorageManager.save(key, value)
+sessionStorageManager.retrieve(key)
+sessionStorageManager.remove(key)
